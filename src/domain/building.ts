1c7c7e4ed2fc30e26773e6c38f8ae0df5c81f028
David Rojo <davidrojo1997@gmail.com>
2022-02-26 23:34:21 +0100

    feat(app): add building class signature

---
+++ src/domain/building.ts
@@ -0,0 +1,29 @@
+import { Elevator } from "./elevator";
+
+export class Building {
+  private elevators: Elevator[] = [];
+  constructor(private _name: string) {}
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  public addElevator(elevator: Elevator) {
+    this.elevators = [...this.elevators, elevator];
+  }
+
+  public removeElevator(elevator: Elevator) {
+    this.elevators = this.elevators.filter(
+      (buildingElevator) =>
+        elevator.serialNumber !== buildingElevator.serialNumber
+    );
+  }
+
+  public toString(): string {
+    return "";
+  }
+}
