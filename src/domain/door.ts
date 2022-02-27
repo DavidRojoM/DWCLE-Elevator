fc4e6970bd7b30595cb76a43868dd9f86cf354e9
David Rojo <davidrojo1997@gmail.com>
2022-02-27 02:15:29 +0100

    feat(app): add door setOpen impl

--- src/domain/door.ts
+++ src/domain/door.ts
@@ -3,12 +3,19 @@ export class Door {
 
   constructor() {}
 
-  get open(): boolean {
-    return this._open;
+  isOpen(): boolean {
+    return this.open;
   }
 
-  set open(value: boolean) {
-    this._open = value;
+  setOpen(open: boolean) {
+    console.log((open ? "opening" : "closing") + " the door...");
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.open = open;
+        console.log("Door " + open ? "opened" : "closed");
+        resolve(open);
+      }, 2000);
+    });
   }
 
   public toString() {
